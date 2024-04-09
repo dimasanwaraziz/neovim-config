fc46953946375128b399fb2273f6e0afb228a540
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2024-04-10 01:43:37 +0700

    set capabilities
    set relative number by default

--- init.lua
+++ init.lua
@@ -6,8 +6,8 @@ vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set expandtab")
 vim.cmd("set clipboard+=unnamedplus")
--- vim.wo.relativenumber = true
-vim.wo.number = true
+vim.wo.relativenumber = true
+-- vim.wo.number = true
 vim.cmd([[
     set autoindent
     set expandtab
