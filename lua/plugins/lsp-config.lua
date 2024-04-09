56e2268de6fcb6c9d08bb20a4a844dcae3144b0a
Dimas Anwar Aziz <dimasanwaraziz@gmail.com>
2024-04-10 01:29:56 +0700

    add feature
    completions for any language, maybe. Hehe.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,10 +16,18 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.yamlls.setup({})
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.yamlls.setup({
+				capabilities = capabilities,
+			})
 
 			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
